e6010eb9e4ca85457f87736b7b0ef0b49bd95528
Liju George <liju.g@fayausa.com>
2017-04-07 11:51:45 +0530

    Product custom form with multiple fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms'; 
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero/hero-detail.component';
+import { ProductComponent }  from './product/product.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    ProductComponent
   ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'product',
+  templateUrl: './product-component.html',
+})
+export class ProductComponent implements OnInit {
+    public myForm: FormGroup;
+
+    constructor(private _fb: FormBuilder) { }
+
+    ngOnInit() {
+        this.myForm = this._fb.group({
+            product: this._fb.array([])
+        });
+        this.addReasons();
+    }
+
+    initQcReject() {
+        return this._fb.group({
+            reason: ['', Validators.required],
+            qty: ['']
+        });
+    }
+
+    addReasons() {
+        const control = <FormArray>this.myForm.controls['product'];
+        const addrCtrl = this.initQcReject();
+        control.push(addrCtrl);
+    }
+
+    removeReasons(i: number) {
+        const control = <FormArray>this.myForm.controls['product'];
+        control.removeAt(i);
+    }
+
+    save() {
+        // call API to save
+        // ...
+        console.log(this.myForm.value);
+    }
+}
