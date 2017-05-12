5519e5a4354cee4b244be7b65648fb051ae31943
Liju George <liju.g@fayausa.com>
2017-05-12 14:03:30 +0530

    Product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,6 +12,7 @@ export class ProductComponent implements OnInit {
 
     ngOnInit() {
         this.myForm = this._fb.group({
+            name: ['', [Validators.required, Validators.minLength(5)]],
             product: this._fb.array([])
         });
         this.addReasons();
